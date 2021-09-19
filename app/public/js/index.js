0ae8e0b5788161941b9af5ed67a3d26f944c7106
phuddar <88210068+phuddar@users.noreply.github.com>
2021-09-19 00:24:49 -0400

    Fetching user data from api
    Fetching user data from randomuser.me

---
+++ app/public/js/index.js
@@ -0,0 +1,26 @@
+const SomeApp = {
+    data() {
+      return {
+        "person": {},
+        }
+    },
+    created() {
+        console.log("A");
+
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            console.log("C");
+        })
+        .catch( err => {
+            console.error(err)
+        })
+
+        console.log("B");
+    }
+  }
+
+Vue.createApp(SomeApp).mount('#someApp');
+Vue.createApp(SomeApp).mount('#someApp2');
